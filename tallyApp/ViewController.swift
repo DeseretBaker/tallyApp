2336fca1eb6c8b2bc7144d8051a4b02bea372959
Deseret Baker <bakdes72@gmail.com>
2024-03-06 19:11:55 -0700

    Initial Commit

---
+++ tallyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tallyApp
+//
+//  Created by Deseret Baker on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
